61c03a97392ceae58ca277ea8124c76acd9aee90
eric <161600909+eric-in-github@users.noreply.github.com>
2024-02-28 13:29:51 +0000

    solve exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,14 @@
-# Exercise 1
-# Your solution comes here
+numbers = input("Enter 5 numbers: ")
+list = []
+for number in numbers:
+	number = int(number)
+	list.append(number)
+
+a = list [0] + list[2] + list[4]
+
+
+b = list[1] + list[3]
+
+final = str(a) + str(b)
+
+print(final)
