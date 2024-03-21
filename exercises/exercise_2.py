46ac95996cb3a2ca93b416730a0f8ab118674f07
eric <161600909+eric-in-github@users.noreply.github.com>
2024-03-21 10:50:01 +0000

    lol

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -3,12 +3,12 @@
 num = int(input("enter a number: "))
 
 even = 0
-odd = 0
+od = 0
 
 if num % 2 == 0:
 	even = num + 2
 	print(even)
 
 else:
-	odd = num + 1
-	print(odd)
+	od = num + 1
+	print(od)
